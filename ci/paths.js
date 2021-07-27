691790bc4a1ed70863f08db5d3a9231e38e24962
dev-front <dev-front@hotmail.com>
2021-07-27 08:50:06 +0800

    修改：     ci/paths.js 	重命名：   ci/webpack/common.conf.js -> ci/webpack/webpack.common.js 	重命名：   ci/webserver.js -> ci/webpack/webpack.dev.js 	新文件：   ci/webpack/webpack.prod.js 	修改：     package.json 	修改：     postcss.config.js 	新文件：   tailwind.config.js

--- ci/paths.js
+++ ci/paths.js
@@ -2,9 +2,10 @@ const path = require('path')
 
 module.exports = {
   R: (...p) => path.resolve(...p),
+  join: (...p) => path.join(...p),
   favicon: path.resolve(__dirname, '../src/assets/icons/logo.png'),
   configBase: path.resolve(__dirname, '../config'),
   src: path.resolve(__dirname, '../src'),
   pub: path.resolve(__dirname, '../public'),
-  build: path.resolve(__dirname, '../build'),
+  build: path.resolve(__dirname, '../dist'),
 }
